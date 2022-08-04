6a4567e75ab904b1c68ef68cf6e773813735fe8a
tongzhenyu <tongzhenyu@hellobike.com>
2022-08-04 16:56:50 +0800

    react

---
+++ src/react/packages/react-dom/src/client/ReactDOMLegacy.js
@@ -0,0 +1,502 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Container} from './ReactDOMHostConfig';
+import type {RootType} from './ReactDOMRoot';
+import type {ReactNodeList} from 'shared/ReactTypes';
+
+import {
+  getInstanceFromNode,
+  isContainerMarkedAsRoot,
+  unmarkContainerAsRoot,
+} from './ReactDOMComponentTree';
+import {createLegacyRoot, isValidContainer} from './ReactDOMRoot';
+import {ROOT_ATTRIBUTE_NAME} from '../shared/DOMProperty';
+import {
+  DOCUMENT_NODE,
+  ELEMENT_NODE,
+  COMMENT_NODE,
+} from '../shared/HTMLNodeType';
+
+import {
+  findHostInstanceWithNoPortals,
+  updateContainer,
+  unbatchedUpdates,
+  getPublicRootInstance,
+  findHostInstance,
+  findHostInstanceWithWarning,
+} from 'react-reconciler/inline.dom';
+import getComponentName from 'shared/getComponentName';
+import invariant from 'shared/invariant';
+import ReactSharedInternals from 'shared/ReactSharedInternals';
+import {has as hasInstance} from 'shared/ReactInstanceMap';
+
+const ReactCurrentOwner = ReactSharedInternals.ReactCurrentOwner;
+
+let topLevelUpdateWarnings;
+let warnedAboutHydrateAPI = false;
+
+if (__DEV__) {
+  topLevelUpdateWarnings = (container: Container) => {
+    if (container._reactRootContainer && container.nodeType !== COMMENT_NODE) {
+      const hostInstance = findHostInstanceWithNoPortals(
+        container._reactRootContainer._internalRoot.current,
+      );
+      if (hostInstance) {
+        if (hostInstance.parentNode !== container) {
+          console.error(
+            'render(...): It looks like the React-rendered content of this ' +
+              'container was removed without using React. This is not ' +
+              'supported and will cause errors. Instead, call ' +
+              'ReactDOM.unmountComponentAtNode to empty a container.',
+          );
+        }
+      }
+    }
+
+    const isRootRenderedBySomeReact = !!container._reactRootContainer;
+    const rootEl = getReactRootElementInContainer(container);
+    const hasNonRootReactChild = !!(rootEl && getInstanceFromNode(rootEl));
+
+    if (hasNonRootReactChild && !isRootRenderedBySomeReact) {
+      console.error(
+        'render(...): Replacing React-rendered children with a new root ' +
+          'component. If you intended to update the children of this node, ' +
+          'you should instead have the existing children update their state ' +
+          'and render the new components instead of calling ReactDOM.render.',
+      );
+    }
+
+    if (
+      container.nodeType === ELEMENT_NODE &&
+      ((container: any): Element).tagName &&
+      ((container: any): Element).tagName.toUpperCase() === 'BODY'
+    ) {
+      console.error(
+        'render(): Rendering components directly into document.body is ' +
+          'discouraged, since its children are often manipulated by third-party ' +
+          'scripts and browser extensions. This may lead to subtle ' +
+          'reconciliation issues. Try rendering into a container element created ' +
+          'for your app.',
+      );
+    }
+  };
+}
+
+function getReactRootElementInContainer(container: any) {
+  if (!container) {
+    return null;
+  }
+
+  if (container.nodeType === DOCUMENT_NODE) {
+    return container.documentElement;
+  } else {
+    return container.firstChild;
+  }
+}
+
+function shouldHydrateDueToLegacyHeuristic(container) {
+  const rootElement = getReactRootElementInContainer(container);
+  return !!(
+    rootElement &&
+    rootElement.nodeType === ELEMENT_NODE &&
+    rootElement.hasAttribute(ROOT_ATTRIBUTE_NAME)
+  );
+}
+
+/**
+ * 判断是否为服务器端渲染 如果不是服务器端渲染
+ * 清空 container 容器中的节点
+ */
+function legacyCreateRootFromDOMContainer(
+  container: Container,
+  forceHydrate: boolean,
+): RootType {
+  // container => <div id="root"></div>
+  // 检测是否为服务器端渲染
+  const shouldHydrate =
+    forceHydrate || shouldHydrateDueToLegacyHeuristic(container);
+  // 如果不是服务器端渲染
+  if (!shouldHydrate) {
+    let warned = false;
+    let rootSibling;
+    // 开启循环 删除 container 容器中的节点
+    while ((rootSibling = container.lastChild)) {
+      // 在开发环境中
+      if (__DEV__) {
+        /**
+         * 判断子节点是否为元素节点, 并且元素节点不能有 data-reactroot 属性, 否则报错
+         * 通常在服务器端渲染时会遇到这个问题
+         *
+         * <div id="app">
+         *  <%- markup %>
+         * </div>
+         * ↑ 报错
+         *
+         * <div id="app"><%- markup %></div>
+         * ↑ 不报错 删除了所有空白区域
+         */
+        if (
+          !warned &&
+          rootSibling.nodeType === ELEMENT_NODE &&
+          // 判断是否是服务端渲染的标志
+          (rootSibling: any).hasAttribute(ROOT_ATTRIBUTE_NAME)
+        ) {
+          warned = true;
+          console.error(
+            'render(): Target node has markup rendered by React, but there ' +
+              'are unrelated nodes as well. This is most commonly caused by ' +
+              'white-space inserted around server-rendered markup.',
+          );
+        }
+      }
+      // 删除 container 容器中的节点
+      container.removeChild(rootSibling);
+      /**
+       * 为什么要清除 container 中的元素 ?
+       * 有时需要在 container 中放置一些占位图或者 loading 图以提高首屏加载用户体验,
+       * 就无可避免的要向 container 中加入 html 标记.
+       * 在将 ReactElement 渲染到 container 之前, 必然要先清空 container
+       * 因为占位图和 ReactElement 不能同时显示
+       *
+       * 在加入占位代码时, 最好只有一个父级元素, 可以减少内部代码的循环次数以提高性能
+       * <div>
+       *  <p>placement<p>
+       *  <p>placement<p>
+       *  <p>placement<p>
+       * </div>
+       */
+    }
+  }
+  // 在开发环境下
+  if (__DEV__) {
+    // 如果是服务器端渲染 控制台提示错误
+    if (shouldHydrate && !forceHydrate && !warnedAboutHydrateAPI) {
+      warnedAboutHydrateAPI = true;
+      // 在客户端二次渲染时不要使用 render 方法, 要使用 hydrate 方法替代
+      console.warn(
+        'render(): Calling ReactDOM.render() to hydrate server-rendered markup ' +
+          'will stop working in React v17. Replace the ReactDOM.render() call ' +
+          'with ReactDOM.hydrate() if you want React to attach to the server HTML.',
+      );
+    }
+  }
+  return createLegacyRoot(
+    container,
+    shouldHydrate
+      ? {
+          hydrate: true,
+        }
+      : undefined,
+  );
+}
+
+function warnOnInvalidCallback(callback: mixed, callerName: string): void {
+  if (__DEV__) {
+    if (callback !== null && typeof callback !== 'function') {
+      console.error(
+        '%s(...): Expected the last optional `callback` argument to be a ' +
+          'function. Instead received: %s.',
+        callerName,
+        callback,
+      );
+    }
+  }
+}
+/**
+ * 将子树渲染到容器中 (初始化 Fiber 数据结构: 创建 fiberRoot 及 rootFiber)
+ * parentComponent: 父组件, 初始渲染传入了 null
+ * children: render 方法中的第一个参数, 要渲染的 ReactElement
+ * container: 渲染容器
+ * forceHydrate: true 为服务端渲染, false 为客户端渲染
+ * callback: 组件渲染完成后需要执行的回调函数
+ **/
+function legacyRenderSubtreeIntoContainer(
+  parentComponent: ?React$Component<any, any>,
+  children: ReactNodeList,
+  container: Container,
+  forceHydrate: boolean,
+  callback: ?Function,
+) {
+  if (__DEV__) {
+    topLevelUpdateWarnings(container);
+    warnOnInvalidCallback(callback === undefined ? null : callback, 'render');
+  }
+
+  /**
+   * 检测 container 是否已经是初始化过的渲染容器
+   * react 在初始渲染时会为最外层容器添加 _reactRootContainer 属性
+   * react 会根据此属性进行不同的渲染方式
+   * root 不存在 表示初始渲染
+   * root 存在 表示更新
+   */
+  // 获取 container 容器对象下是否有 _reactRootContainer 属性
+  let root: RootType = (container._reactRootContainer: any);
+  // 即将存储根 Fiber 对象
+  let fiberRoot;
+  if (!root) {
+    // 初始渲染
+    // 初始化根 Fiber 数据结构
+    // 为 container 容器添加 _reactRootContainer 属性
+    // 在 _reactRootContainer 对象中有一个属性叫做 _internalRoot
+    // _internalRoot 属性值即为 FiberRoot 表示根节点 Fiber 数据结构
+    // legacyCreateRootFromDOMContainer
+    // createLegacyRoot
+    // new ReactDOMBlockingRoot -> this._internalRoot
+    // createRootImpl
+    root = container._reactRootContainer = legacyCreateRootFromDOMContainer(
+      container,
+      forceHydrate,
+    );
+    // 获取 Fiber Root 对象
+    fiberRoot = root._internalRoot;
+    // console.log("🟢 [Logger:]  🟦 fiberRoot", fiberRoot);
+    /**
+     * 改变 callback 函数中的 this 指向
+     * 使其指向 render 方法第一个参数的真实 DOM 对象
+     */
+    // 如果 callback 参数是函数类型
+    if (typeof callback === 'function') {
+      // 使用 originalCallback 存储 callback 函数
+      const originalCallback = callback;
+      // 为 callback 参数重新赋值
+      callback = function () {
+        // 获取 render 方法第一个参数的真实 DOM 对象
+        // 实际上就是 id="root" 的 div 的子元素
+        // rootFiber.child.stateNode
+        // rootFiber 就是 id="root" 的 div
+        const instance = getPublicRootInstance(fiberRoot);
+        // 调用原始 callback 函数并改变函数内部 this 指向
+        originalCallback.call(instance);
+      };
+    }
+    // 初始化渲染不执行批量更新
+    // 因为批量更新是异步的是可以被打断的, 但是初始化渲染应该尽快完成不能被打断
+    // 所以不执行批量更新
+    unbatchedUpdates(() => {
+      updateContainer(children, fiberRoot, parentComponent, callback);
+    });
+  } else {
+    // 非初始化渲染 即更新
+    fiberRoot = root._internalRoot;
+    if (typeof callback === 'function') {
+      const originalCallback = callback;
+      callback = function () {
+        const instance = getPublicRootInstance(fiberRoot);
+        originalCallback.call(instance);
+      };
+    }
+    // Update
+    updateContainer(children, fiberRoot, parentComponent, callback);
+  }
+  // 返回 render 方法第一个参数的真实 DOM 对象作为 render 方法的返回值
+  // 就是说渲染谁 返回谁的真实 DOM 对象
+  return getPublicRootInstance(fiberRoot);
+}
+
+export function findDOMNode(
+  componentOrElement: Element | ?React$Component<any, any>,
+): null | Element | Text {
+  if (__DEV__) {
+    let owner = (ReactCurrentOwner.current: any);
+    if (owner !== null && owner.stateNode !== null) {
+      const warnedAboutRefsInRender = owner.stateNode._warnedAboutRefsInRender;
+      if (!warnedAboutRefsInRender) {
+        console.error(
+          '%s is accessing findDOMNode inside its render(). ' +
+            'render() should be a pure function of props and state. It should ' +
+            'never access something that requires stale data from the previous ' +
+            'render, such as refs. Move this logic to componentDidMount and ' +
+            'componentDidUpdate instead.',
+          getComponentName(owner.type) || 'A component',
+        );
+      }
+      owner.stateNode._warnedAboutRefsInRender = true;
+    }
+  }
+  if (componentOrElement == null) {
+    return null;
+  }
+  if ((componentOrElement: any).nodeType === ELEMENT_NODE) {
+    return (componentOrElement: any);
+  }
+  if (__DEV__) {
+    return findHostInstanceWithWarning(componentOrElement, 'findDOMNode');
+  }
+  return findHostInstance(componentOrElement);
+}
+
+export function hydrate(
+  element: React$Node,
+  container: Container,
+  callback: ?Function,
+) {
+  invariant(
+    isValidContainer(container),
+    'Target container is not a DOM element.',
+  );
+  if (__DEV__) {
+    const isModernRoot =
+      isContainerMarkedAsRoot(container) &&
+      container._reactRootContainer === undefined;
+    if (isModernRoot) {
+      console.error(
+        'You are calling ReactDOM.hydrate() on a container that was previously ' +
+          'passed to ReactDOM.createRoot(). This is not supported. ' +
+          'Did you mean to call createRoot(container, {hydrate: true}).render(element)?',
+      );
+    }
+  }
+  // TODO: throw or warn if we couldn't hydrate?
+  return legacyRenderSubtreeIntoContainer(
+    null,
+    element,
+    container,
+    true,
+    callback,
+  );
+}
+/**
+ * 渲染入口
+ * element 要进行渲染的 ReactElement
+ * container 渲染容器
+ * callback 渲染完成后执行的回调函数
+ */
+export function render(
+  element: React$Element<any>,
+  container: Container,
+  callback: ?Function,
+) {
+  // 检测 container 是否是符合要求的渲染容器
+  // 即检测 container 是否是真实的DOM对象
+  // 如果不符合要求就报错
+  invariant(
+    isValidContainer(container),
+    'Target container is not a DOM element.',
+  );
+  // 在开发环境下
+  if (__DEV__) {
+    // 检测 container 是否已经传递给 ReactDOM.createRoot() 方法
+    // 如果已经传递, 则 container 不能再传递给 render 方法
+    // 防止 render 方法和 createRoot 方法重复调用
+    const isModernRoot =
+      isContainerMarkedAsRoot(container) &&
+      container._reactRootContainer === undefined;
+    // 如果 container 已经传递给过 createRoot 方法
+    if (isModernRoot) {
+      // 在控制台报错
+      console.error(
+        'You are calling ReactDOM.render() on a container that was previously ' +
+          'passed to ReactDOM.createRoot(). This is not supported. ' +
+          'Did you mean to call root.render(element)?',
+      );
+    }
+  }
+  return legacyRenderSubtreeIntoContainer(
+    // 父组件 初始渲染没有父组件 传递 null 占位
+    null,
+    element,
+    container,
+    // 是否为服务器端渲染 false 不是服务器端渲染 true 是服务器端渲染
+    false,
+    callback,
+  );
+}
+
+export function unstable_renderSubtreeIntoContainer(
+  parentComponent: React$Component<any, any>,
+  element: React$Element<any>,
+  containerNode: Container,
+  callback: ?Function,
+) {
+  invariant(
+    isValidContainer(containerNode),
+    'Target container is not a DOM element.',
+  );
+  invariant(
+    parentComponent != null && hasInstance(parentComponent),
+    'parentComponent must be a valid React Component',
+  );
+  return legacyRenderSubtreeIntoContainer(
+    parentComponent,
+    element,
+    containerNode,
+    false,
+    callback,
+  );
+}
+
+export function unmountComponentAtNode(container: Container) {
+  invariant(
+    isValidContainer(container),
+    'unmountComponentAtNode(...): Target container is not a DOM element.',
+  );
+
+  if (__DEV__) {
+    const isModernRoot =
+      isContainerMarkedAsRoot(container) &&
+      container._reactRootContainer === undefined;
+    if (isModernRoot) {
+      console.error(
+        'You are calling ReactDOM.unmountComponentAtNode() on a container that was previously ' +
+          'passed to ReactDOM.createRoot(). This is not supported. Did you mean to call root.unmount()?',
+      );
+    }
+  }
+
+  if (container._reactRootContainer) {
+    if (__DEV__) {
+      const rootEl = getReactRootElementInContainer(container);
+      const renderedByDifferentReact = rootEl && !getInstanceFromNode(rootEl);
+      if (renderedByDifferentReact) {
+        console.error(
+          "unmountComponentAtNode(): The node you're attempting to unmount " +
+            'was rendered by another copy of React.',
+        );
+      }
+    }
+
+    // Unmount should not be batched.
+    unbatchedUpdates(() => {
+      legacyRenderSubtreeIntoContainer(null, null, container, false, () => {
+        // $FlowFixMe This should probably use `delete container._reactRootContainer`
+        container._reactRootContainer = null;
+        unmarkContainerAsRoot(container);
+      });
+    });
+    // If you call unmountComponentAtNode twice in quick succession, you'll
+    // get `true` twice. That's probably fine?
+    return true;
+  } else {
+    if (__DEV__) {
+      const rootEl = getReactRootElementInContainer(container);
+      const hasNonRootReactChild = !!(rootEl && getInstanceFromNode(rootEl));
+
+      // Check if the container itself is a React root node.
+      const isContainerReactRoot =
+        container.nodeType === ELEMENT_NODE &&
+        isValidContainer(container.parentNode) &&
+        !!container.parentNode._reactRootContainer;
+
+      if (hasNonRootReactChild) {
+        console.error(
+          "unmountComponentAtNode(): The node you're attempting to unmount " +
+            'was rendered by React and is not a top-level container. %s',
+          isContainerReactRoot
+            ? 'You may have accidentally passed in a React root node instead ' +
+                'of its container.'
+            : 'Instead, have the parent component update its state and ' +
+                'rerender in order to remove this component.',
+        );
+      }
+    }
+
+    return false;
+  }
+}
